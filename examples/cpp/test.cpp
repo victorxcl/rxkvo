0699430a88a37375d9342e56da0e12105ad15b3f
pandaxcl <pandaxcl@gmail.com>
2017-01-03 11:42:23 +0800

    1. 首次实现了kvo_variable的测试用例，并编写了kvo_variable的代码；）测试通过；）

--- examples/cpp/test.cpp
+++ examples/cpp/test.cpp
@@ -1,3 +1,58 @@
 #include <rxcpp/rx.hpp>
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include "kvo_extension.hpp"
+
+SCENARIO("test kvo_varibale", "")
+{
+    GIVEN("a person")
+    {
+        struct Person
+        {
+            kvo_variable<std::string> firstName;
+            kvo_variable<std::string> lastName;
+            kvo_variable<std::string> fullName;
+            
+            Person()
+            {
+                firstName.subject.get_observable()
+                .combine_latest(lastName.subject.get_observable())
+                .subscribe([this](std::pair<std::string,std::string>x){
+                    this->fullName = x.first + " " + x.second;
+                });
+            }
+        }person;
+        
+        REQUIRE(person.firstName.get() == "");
+        REQUIRE(person.lastName.get() == "");
+        REQUIRE(person.fullName.get() == " ");
+        WHEN("modify firstName")
+        {
+            person.firstName = "Hello";
+            REQUIRE(person.firstName.get() == "Hello");
+            REQUIRE(person.lastName.get() == "");
+            REQUIRE(person.fullName.get() == "Hello ");
+            THEN("modify lastName")
+            {
+                person.lastName = "World";
+                REQUIRE(person.firstName.get() == "Hello");
+                REQUIRE(person.lastName.get() == "World");
+                REQUIRE(person.fullName.get() == "Hello World");
+            }
+        }
+        WHEN("modify lastName")
+        {
+            person.lastName = "World";
+            REQUIRE(person.firstName.get() == "");
+            REQUIRE(person.lastName.get() == "World");
+            REQUIRE(person.fullName.get() == " World");
+            THEN("modify firstName")
+            {
+                person.firstName = "Hello";
+                REQUIRE(person.firstName.get() == "Hello");
+                REQUIRE(person.lastName.get() == "World");
+                REQUIRE(person.fullName.get() == "Hello World");
+            }
+        }
+    }
+}
--- src/cpp/kvo_extension.hpp
+++ src/cpp/kvo_extension.hpp
@@ -1,6 +1,26 @@
-class kvo_property
+
+template<typename T>
+class kvo_variable
 {
+    typedef kvo_variable self_t;
+public:
+    self_t&operator = (T&&x)
+    {
+        this->subject.get_subscriber().on_next(x);
+        return *this;
+    }
+    operator T()const
+    {
+        return this->subject.get_value();
+    }
+    T get()const
+    {
+        return this->subject.get_value();
+    }
+    rxcpp::subjects::behavior<T> subject = rxcpp::subjects::behavior<T>(T());
 };
+
+
 class kvo_array
 {
 };
