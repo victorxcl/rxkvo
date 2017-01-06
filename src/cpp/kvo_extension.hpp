459f141d914c349678166ebde8cc398ac6ba7738
pandaxcl <pandaxcl@gmail.com>
2017-01-06 11:39:03 +0800

    1. 将对容器的操作从kvo_collection里面分离出来了！这是采用了AOP的思想，灵活性大为增强：）

--- src/cpp/kvo_extension.hpp
+++ src/cpp/kvo_extension.hpp
@@ -54,10 +54,78 @@ public:
 };
 
 template<typename Collection>
+class operation;
+
+template<typename T>
+class operation<std::vector<T>>
+{
+public:
+    typedef std::vector<T>                              collection_type;
+    typedef typename collection_type::difference_type   difference_type;
+    typedef typename collection_type::const_iterator    const_iterator;
+    typedef typename collection_type::iterator          iterator;
+    typedef std::vector<T>                              rx_notify_value;
+    typedef std::vector<difference_type>                rx_notify_index;
+    
+    static rx_notify_index indices_for_append_items(collection_type&c, const rx_notify_value&x)
+    {
+        rx_notify_index indices;
+        for (difference_type i=0; i<x.size(); i++)
+        {
+            indices.push_back(c.size() + i);
+        }
+        return std::move(indices);
+    }
+    
+    static rx_notify_value items_at_indices(collection_type&c, const rx_notify_index&indices)
+    {
+        rx_notify_value items;
+        for (const auto&i:indices)
+        {
+            auto it = c.begin(); std::advance(it, i);
+            items.push_back(*it);
+        }
+        return std::move(items);
+    }
+    
+    static void set(collection_type&c, const rx_notify_value&x)
+    {
+        c = x;
+    }
+    static void insert(collection_type&c, const rx_notify_value&x, const rx_notify_index&indices)
+    {
+        for (difference_type i=0;i<x.size();i++)
+        {
+            auto it = c.begin(); std::advance(it, indices.at(i));
+            c.insert(it, x.at(i));
+        }
+    }
+    
+    static void remove(collection_type&c, const rx_notify_index&indices)
+    {
+        for (auto it_i=indices.crbegin(); it_i!=indices.crend(); it_i++)
+        {
+            auto it_x = c.begin(); std::advance(it_x, *it_i);
+            c.erase(it_x);
+        }
+    }
+    
+    static void replace(collection_type&c, const rx_notify_index&indices, const rx_notify_value&x)
+    {
+        for (difference_type i=0;i<x.size();i++)
+        {
+            auto it = c.begin(); std::advance(it, indices.at(i));
+            *it = x.at(i);
+        }
+    }
+};
+
+template<typename Collection, typename Operation=operation<Collection>>
 class kvo_collection
 {
 public:
     typedef Collection                                  collection_type;
+    typedef Operation                                   operation_type;
     typedef typename collection_type::value_type        value_type;
     typedef typename collection_type::difference_type   difference_type;
     typedef typename collection_type::const_iterator    const_iterator;
@@ -86,38 +154,18 @@ public:
     rxcpp::subjects::subject<rx_notify_index> subject_removal_index;
     rxcpp::subjects::subject<rx_notify_index> subject_replacement_index;
     
-    collection_type&get()
-    {
-        return this->collection;
-    }
+    collection_type&get() { return this->collection; }
     
-    collection_type& operator()()
-    {
-        return this->get();
-    }
+    collection_type& operator()() { return this->get(); }
     
     void set(const rx_notify_value&x)
     {
         if (this->collection.size() > 0 || (this->collection.size() == 0 && x.size() > 0))
         {
             this->subject_setting_will.get_subscriber().on_next(x);
-            this->collection = x;
+            operation_type::set(this->collection, x);
             this->subject_setting_did.get_subscriber().on_next(x);
         }
-    }
-    
-    const_iterator iterator_at(difference_type i) const
-    {
-        auto it = this->collection.begin();
-        std::advance(it, i);
-        return it;
-    }
-    
-    iterator iterator_at(difference_type i)
-    {
-        auto it = this->collection.begin();
-        std::advance(it, i);
-        return it;
     }
     
     void insert(const rx_notify_value&x, const rx_notify_index&indices)
@@ -129,11 +177,7 @@ public:
             
             subject_insertion_will.get_subscriber().on_next(x);
             
-            for (difference_type i=0;i<x.size();i++)
-            {
-                auto it = iterator_at(indices.at(i));
-                this->collection.insert(it, x.at(i));
-            }
+            operation_type::insert(this->collection, x, indices);
             
             subject_insertion_did.get_subscriber().on_next(x);
         }
@@ -143,9 +187,7 @@ public:
     {
         if (x.size() > 0)
         {
-            rx_notify_index indices;
-            for (difference_type i=0; i<x.size(); i++)
-                indices.push_back(this->collection.size() + i);
+            rx_notify_index indices = operation_type::indices_for_append_items(this->collection, x);
             
             subject_insertion_index.get_subscriber().on_next(indices);
             
@@ -163,16 +205,11 @@ public:
         {
             subject_removal_index.get_subscriber().on_next(indices);
             
-            rx_notify_value items;
-            for (const auto&i:indices)
-                items.push_back(*iterator_at(i));
+            rx_notify_value items = operation_type::items_at_indices(this->collection, indices);
             
             subject_removal_will.get_subscriber().on_next(items);
             
-            for (auto it=indices.crbegin(); it!=indices.crend(); it++)
-            {
-                this->collection.erase(iterator_at(*it));
-            }
+            operation_type::remove(this->collection, indices);
             
             subject_removal_did.get_subscriber().on_next(items);
         }
@@ -192,10 +229,7 @@ public:
             
             subject_replacement_will.get_subscriber().on_next(x);
             
-            for (difference_type i=0;i<x.size();i++)
-            {
-                *iterator_at(indices.at(i)) = x.at(i);
-            }
+            operation_type::replace(this->collection, indices, x);
             
             subject_replacement_did.get_subscriber().on_next(x);
         }
