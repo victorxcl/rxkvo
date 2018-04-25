b0426bce3777c52ef44a656cbc33263f9556838d
pandaxcl <pandaxcl@icloud.com>
2018-04-25 14:30:21 +0800

    1. 完成了第一个plan项目c=a+b的过程xml输出。配合相应的xslt来生成制作keynote的步骤，然后就可以快速制作幻灯片了；）

---
+++ examples/cpp/test_course_network.cpp
@@ -0,0 +1,20 @@
+#include <rxcpp/rx.hpp>
+#include <catch.hpp>
+#include <unordered_map>
+#include "kvo_extension.hpp"
+
+#pragma warning(disable:4503)
+
+namespace mail{
+}
+
+SCENARIO("rx course network request", "")
+{
+    GIVEN("")
+    {
+        THEN("")
+        {
+        }
+    }
+}
+
--- examples/cpp/test_plan.cpp
+++ examples/cpp/test_plan.cpp
@@ -5,15 +5,86 @@
 
 #pragma warning(disable:4503)
 
-namespace mail{
-}
+#define SHOULE_DUMP_XML    0
 
-SCENARIO("the projector's plan client", "")
+SCENARIO("the first plan case for rxkvo", "")
 {
-    GIVEN("")
+    GIVEN("three numbers: a, b, c")
     {
-        THEN("")
+        struct Plan
         {
+            std::atomic<int> t{0};
+            kvo::variable<int> a{1};
+            kvo::variable<int> b{2};
+            kvo::variable<int> c{0};
+            kvo::variable<std::string> statement;
+            
+            Plan()
+            {
+#if SHOULE_DUMP_XML
+                using namespace std::literals::string_literals;
+                statement.subject.get_observable()
+                .start_with("<plan expression=\"c=a+b\">"s)
+                .finally([](){ std::cout<<"</plan>"<<std::endl; })
+                .subscribe([](auto x){ std::cout<<x<<std::endl; });
+                
+                a.subject.get_observable().subscribe([this](auto x){
+                    statement="<report variable=\"a\" t=\""+std::to_string(t)+"\">"+std::to_string(x)+"</report>";
+                });
+                b.subject.get_observable().subscribe([this](auto x){
+                    statement="<report variable=\"b\" t=\""+std::to_string(t)+"\">"+std::to_string(x)+"</report>";
+                });
+                c.subject.get_observable().subscribe([this](auto x){
+                    statement="<report variable=\"c\" t=\""+std::to_string(t)+"\">"+std::to_string(x)+"</report>";
+                });
+#endif
+            }
+            ~Plan() { statement.subject.get_subscriber().on_completed(); }
+            
+            void create_expression()
+            {
+                t++;
+                auto expr = u8R"===(<plus><variable name="a"/><variable name="b"/></plus>)===";
+                statement = "<assign variable=\"c\" t=\""+ std::to_string(t) + "\">" + expr + "</assign>";
+                a.subject.get_observable()
+                .combine_latest(b.subject.get_observable())
+                .subscribe([this](auto&&x){
+                    c = std::get<0>(x) + std::get<1>(x);
+                });
+            }
+            
+            void assign_a_with(int v)
+            {
+                t++;
+                statement = "<assign variable=\"a\" t=\""+ std::to_string(t) + "\">" + std::to_string(v) + "</assign>";
+                a = v;
+            }
+            void assign_b_with(int v)
+            {
+                t++;
+                statement = "<assign variable=\"b\" t=\""+ std::to_string(t) + "\">" + std::to_string(v) + "</assign>";
+                b = v;
+            }
+        }plan;
+
+        REQUIRE(1 == plan.a);
+        REQUIRE(2 == plan.b);
+        REQUIRE(0 == plan.c);
+        
+        THEN("c subscribe to a+b")
+        {
+            plan.create_expression();
+            REQUIRE(1+2 == plan.c);
+            THEN("assign 10 to a")
+            {
+                plan.assign_a_with(10);
+                REQUIRE(10+2 == plan.c);
+                AND_THEN("assign 20 to b")
+                {
+                    plan.assign_b_with(20);
+                    REQUIRE(10+20 == plan.c);
+                }
+            }
         }
     }
 }
